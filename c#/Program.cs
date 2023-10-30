399cf8d94503d6ab599ff8d9b4c3aa39effeb773
Tunec <112854325+Tunec6@users.noreply.github.com>
2023-10-31 02:53:12 +0500

    Add files via upload

---
+++ c#/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Net;
+
+class garden{
+
+    static int [,] pole;
+    static void Main(){
+
+        pole = new int [10,10];
+
+        Thread A = new Thread(gardenerA);
+        Thread B = new Thread(gardenerB);
+
+        A.Start();
+        B.Start();
+        A.Join();
+        B.Join();
+
+          for (int i = 0; i < 10; i++)
+            {
+                for (int a = 0; a < 10; a++)
+                {
+                    Console.Write(pole[i, a] + " ");
+                }
+                Console.WriteLine();
+            }
+
+
+    }
+
+    static void gardenerA(){
+        for(int i = 0; i < 10; i++ ){
+            for(int a = 0; a < 10; a ++){
+                if(pole[i,a] == 0){
+                    pole[i,a] = 1;
+                }
+                Thread.Sleep(1);
+            }
+        }
+    }
+
+    static void gardenerB(){
+        for(int i = 10 - 1; i > 0; i--){
+            for(int b = 10 - 1; b > 0; b--){
+                if(pole[i,b] == 0){
+                    pole[i,b]= 2;
+                }
+                Thread.Sleep(1);
+            }
+        }
+    }
+
+
+
+}
